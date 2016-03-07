858846fef94ae613ae31568dc5da21b396e32fab
cuipengfei <cuipengfei2008@gmail.com>
2016-03-07 11:09:47 +0800

    require local file instead of from npm

--- apkupdater.js
+++ apkupdater.js
@@ -1,11 +1,11 @@
-var apkUpdater = require('apk-updater');
+var apkUpdater = require('./lib/updater');
 var express = require('express');
 
-var app = express()
+var app = express();
 app.use(express.bodyParser());
 
 apkUpdater.enable(app, '/', './apks');
 
 var server = app.listen(8090, '127.0.0.1', function () {
-  console.log('apk updater running');
+    console.log('apk updater running');
 });
